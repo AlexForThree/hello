779b46aabe8ac59b0e828a7d38ec221157c3b197
Lijj <1239392670@qq.com>
2020-07-08 15:11:27 +0800

    修改le

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("HelloWord!");
-        System.out.println("h");
+        System.out.println("hi");
     }
 }
